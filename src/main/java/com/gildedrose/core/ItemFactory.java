6a455b749cde39dd08204136c0dd5bbbba0108d0
Nycto97 <jelle.van.goethem@hotmail.com>
2021-12-20 20:09:13 +0100

    Added new switch item Added SwitchItem class + test class

--- src/main/java/com/gildedrose/core/ItemFactory.java
+++ src/main/java/com/gildedrose/core/ItemFactory.java
@@ -5,21 +5,31 @@ import com.gildedrose.items.*;
 public class ItemFactory {
 
     private static ItemFactory instance = null;
-    private ItemFactory() {}
+
+    private ItemFactory() {
+    }
 
     public static ItemFactory getInstance() {
-        if (instance == null) instance =  new ItemFactory();
+        if (instance == null) instance = new ItemFactory();
         return instance;
     }
 
     public Item createItem(String name, int sellIn, int quality) throws Exception {
-        switch(name) {
-            case "+5 Dexterity Vest": return new NormalItem(name, sellIn, quality);
-            case "Aged Brie": return new AgingItem(name, sellIn, quality);
-            case "Elixir of the Mongoose": return new NormalItem(name, sellIn, quality);
-            case "Sulfuras, Hand of Ragnaros": return new LegendaryItem(name, sellIn, quality);
-            case "Backstage passes to a TAFKAL80ETC concert": return new TicketItem(name, sellIn, quality);
-            case "Conjured Mana Cake": return new ConjuredItem(name, sellIn, quality);
+        switch (name) {
+            case "+5 Dexterity Vest":
+                return new NormalItem(name, sellIn, quality);
+            case "Aged Brie":
+                return new AgingItem(name, sellIn, quality);
+            case "Elixir of the Mongoose":
+                return new NormalItem(name, sellIn, quality);
+            case "Sulfuras, Hand of Ragnaros":
+                return new LegendaryItem(name, sellIn, quality);
+            case "Backstage passes to a TAFKAL80ETC concert":
+                return new TicketItem(name, sellIn, quality);
+            case "Conjured Mana Cake":
+                return new ConjuredItem(name, sellIn, quality);
+            case "+5 Test Article":
+                return new SwitchItem(name, sellIn, quality);
         }
         throw new Exception("My apologies, but we are not selling this type of item with the name: " + name);
     }
---
+++ src/main/java/com/gildedrose/items/SwitchItem.java
@@ -0,0 +1,23 @@
+package com.gildedrose.items;
+
+import com.gildedrose.core.Item;
+
+public class SwitchItem extends Item {
+
+    public SwitchItem(String name, int sellIn, int quality) {
+        super(name, sellIn, quality);
+    }
+
+    @Override
+    public void updateQuality() {
+        if (sellIn >= 0) {
+            if (quality < 50) {
+                quality++;
+            }
+        }
+        if (sellIn < 0) {
+            if (quality > 0)
+                quality--;
+        }
+    }
+}
